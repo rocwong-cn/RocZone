51e795bea728127c3e4d73d17d6f3811c5d02f21
RocWang <rocwangi@sina.cn>
2018-05-08 14:49:15 +0800

    a simple home page init.

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <p class="bei-an">京ICP备18022839号</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    width: 100%;
+    overflow: hidden;
+  }
+
+  .bei-an {
+    color: #666;
+    font-size: 12px;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="hello">
+    <h2 class="animated bounceInDown">{{ msg }}</h2>
+    <ul>
+      <li class="animated bounceInLeft">
+        <a
+          href="https://github.com/rocwangv"
+          target="_blank"
+        >
+          Github
+        </a>
+      </li>
+      <li class="animated bounceInUp">
+        <a
+          href="http://rocwang.cc"
+          target="_blank"
+        >
+          Blog
+        </a>
+      </li>
+      <li class="animated bounceInRight">
+        <a
+          href="http://weibo.com/pengwang2012"
+          target="_blank"
+        >
+          WeiBo
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Roc\'s Zone !'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+    text-decoration: none;
+  }
+
+  .hello {
+    position: absolute;
+    top: 40%;
+    width: 100%;
+  }
+</style>
